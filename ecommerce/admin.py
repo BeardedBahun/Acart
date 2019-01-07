5534b3b12f82fcbf304f45439c5edff85628b39b
Hari Bhandari <bhandarihari07@gmail.com>
2019-01-08 05:38:38 +0545

    admin site registered for ecommerce

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from ecommerce.models import Category, Product, ProductHasImage, ProductReview, WishList, Cart
+
+admin.site.register(Category)
+
+admin.site.register(Product)
+
+admin.site.register(ProductReview)
+
+admin.site.register(ProductHasImage)
+
+admin.site.register(WishList)
+
+admin.site.register(Cart)
