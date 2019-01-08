841807adfc7a778bc59e95be5364b529b20d7fda
Hari Bhandari <bhandarihari07@gmail.com>
2019-01-08 07:08:33 +0545

    nothing

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 
 from ecommerce.models import Category, Product, ProductHasImage, ProductReview, WishList, Cart
 
+
+class ProductHasImageInline(admin.TabularInline):
+    model = ProductHasImage
+
+
+class AdminProduct(admin.ModelAdmin):
+    inlines = [ProductHasImageInline]
+
+
 admin.site.register(Category)
 
-admin.site.register(Product)
-
+admin.site.register(Product, AdminProduct)  # product sangai image pani upload garna milxa admin panal ma
+# it merges Product and ProductHasImage in. ProductHas image is child of parent Product.Check django admin tabular inline
 admin.site.register(ProductReview)
 
 admin.site.register(ProductHasImage)
