97291eb6a899d619816ab7169ca6f34db4e77e06
Bishnu Bhusal <bishnu@hashunited.com>
2019-01-08 07:05:54 +0545

    gitignore edited

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -2,13 +2,22 @@ from django.contrib import admin
 
 from ecommerce.models import Category, Product, ProductHasImage, ProductReview, WishList, Cart
 
+
+class ProductHasImageInline(admin.TabularInline):
+    model = ProductHasImage
+
+
+class AdminProduct(admin.ModelAdmin):
+    inlines = [ProductHasImageInline]
+
+
 admin.site.register(Category)
 
-admin.site.register(Product)
+admin.site.register(Product, AdminProduct)
 
 admin.site.register(ProductReview)
 
-admin.site.register(ProductHasImage)
+# admin.site.register(ProductHasImage)
 
 admin.site.register(WishList)
 
